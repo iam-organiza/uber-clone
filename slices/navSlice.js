685a7911b8f67b0f4fd65d890a7d2e7ca6c3d9c6
Samuel Organizer <organizersamuel.os@gmail.com>
2023-07-09 13:40:48 +0100

    setup redux toolkit done

---
+++ slices/navSlice.js
@@ -0,0 +1,33 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+    origin: null,
+    destination: null,
+    travelTimeInformation: null,
+};
+
+export const counterSlice = createSlice({
+    name: 'nav',
+    initialState,
+    reducers: {
+        setOrigin(state, action) {
+            state.origin = action.payload;
+        },
+
+        setDestination(state, action) {
+            state.destination = action.payload;
+        },
+
+        setTravelTimeInformation(state, action) {
+            state.travelTimeInformation = action.payload;
+        }
+    },
+});
+
+export const { setOrigin, setDestination, setTravelTimeInformation } = counterSlice.actions;
+
+export const selectOrigin = state => state.nav.origin;
+export const selectDestination = state => state.nav.destination;
+export const selectTravelTimeInformation = state => state.nav.travelTimeInformation;
+
+export default counterSlice.reducer;
